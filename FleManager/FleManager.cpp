9255cdb6d5f2d34c6d739b02f4ab9f67839fd416
Zokerzes <romzes85@mail.ru>
2022-08-17 18:43:28 +0300

    ++

--- FleManager/FleManager.cpp
+++ FleManager/FleManager.cpp
@@ -1,4 +1,5 @@
-﻿//Здесь находятся функции переименования и удаления
+﻿#define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
+//Здесь находятся функции переименования и удаления
 #include <stdio.h>
 //Здесь находятся функции для поиска файлов
 #include <io.h>
@@ -8,16 +9,53 @@
 #include "Directory.h"
 #include "workingWith.h"
 #include "Menu.h"
-using namespace std;
+#include <experimental/filesystem>
 
+using namespace std;
+namespace fs = std::experimental::filesystem;
 
 int main()
 {
 	
 	//File f;
 	//Directory d;
-	Menu menu;
+	//Menu menu;
+	// Получаем текущий путь процесса (a.k.a. working directory)
+	//const fs::path workdir = fs::current_path();
+	//cout << workdir<<endl;
+	//fs::create_directories("U:\\FleManager\\FleManager\\112"); //worcked!!
+	std::vector<fs::path> paths = {
+		"U:\\FleManager\\FleManager\\112\\1.txt",
+		"U:\\FleManager\\FleManager\\112\\2.txt",
+		"U:\\FleManager\\FleManager\\112\\3.dat",
+		"U:\\FleManager\\FleManager\\112\\4.dat"
+	};
+	for (const auto& filepath : paths)
+	{
+		// В C++17 конструктор fstream принимает как string, так и wstring,
+		//  это упрощает кроссплатформенное конструирование его из path
+		std::ofstream(filepath.native()) << "test";
+	}
 
+	fs::recursive_directory_iterator begin("data");
+	fs::recursive_directory_iterator end;
+	// Получаем список подкаталогов с помощью алгоритма copy_if
+	std::vector<fs::path> subdirs;
+	std::copy_if(begin, end, std::back_inserter(subdirs), [](const fs::path& path) {
+		return fs::is_directory(path);
+		});
+	// Выводим список подкаталогов в cout.
+	std::cout << "-- subdirs --" << std::endl;
+	std::copy(subdirs.begin(), subdirs.end(), std::ostream_iterator<fs::path>(std::cout, "\n"));
+
+
+
+
+
+
+	/*fs::current_path() = "U:\\FleManager\\FleManager\\111";
+	fs::path workdir = fs::current_path();
+	cout << workdir;*/
 	//cout<<"fil_cr "<< f.create("tes1.txt") << endl;
 	/*cout << "fil_cr " << f.create("test.txt","some thing") << endl;
 	cout<< "dir_cr " << d.create("d1") << endl;
